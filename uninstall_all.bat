7700afbb8777153dd83ceefe9b5b3583d3913ece
VoltXd <pierrealexandre.peyronnet@gmail.com>
2023-02-23 19:12:01 +0100

    Update uninstall_all.bat

--- uninstall_all.bat
+++ uninstall_all.bat
@@ -1,4 +1,14 @@
 @echo off
+echo "*********** Warning ***********"
+echo "You are going to uninstall the project build and it's external components (SDL2 and OpenCV)."
+
+:choice
+set /P c=Proceed? (y/n)
+if /I "%c%" EQU "y" goto :Yes
+if /I "%c%" EQU "n" goto :No
+goto :choice
+
+:Yes
 echo Remove ./build
 RD /S /Q "./build"
 echo Remove C:/lib_visual_cpp/SDL2
@@ -9,3 +19,5 @@ echo Remove C:/lib_visual_cpp/opencv_4.7.0
 RD /S /Q "C:/lib_visual_cpp/opencv_4.7.0"
 echo Remove C:/lib_cpp/opencv_4.7.0
 RD /S /Q "C:/lib_cpp/opencv_4.7.0"
+
+:No
