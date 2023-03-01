3e1f3a9fa76694ec01c76e0477029e626c97b03c
VoltXd <pierrealexandre.peyronnet@gmail.com>
2023-03-01 17:52:17 +0100

    Improved scripts
    Enables you to choose a drive for installation and uninstallation

--- uninstall_all.bat
+++ uninstall_all.bat
@@ -9,15 +9,51 @@ if /I "%c%" EQU "n" goto :No
 goto :choice
 
 :Yes
+
+:: ______________Choosing disk______________ ::
+
+:: List availables disks
+set disks=
+
+:: Loop on all possible disks
+SETLOCAL EnableDelayedExpansion
+for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do call :foo %%a 
+goto end
+
+:foo
+if exist %1:\nul (
+    :: If the disk exists, add it to the disks string
+    set disks=!disks!%1 
+)
+:: Return to the for loop
+goto :eof
+
+:: End of for loop
+:end
+
+:: Ask the user to select a disk
+:choice
+set /P disk=Select a disk (%disks%)
+call set disks_without_selection=%%disks:%disk%=%%
+if not x%disks_without_selection%==x%disks% (
+    :: The disk is in the available disks string => OK
+    goto :OK
+) else (
+    :: Else => retry
+    goto :choice
+)
+
+:OK
+
 echo Remove ./build
 RD /S /Q "./build"
-echo Remove C:/lib_visual_cpp/SDL2
-RD /S /Q "C:/lib_visual_cpp/SDL2"
-echo Remove C:/lib_cpp/SDL2
-RD /S /Q "C:/lib_cpp/SDL2"
-echo Remove C:/lib_visual_cpp/opencv_4.7.0
-RD /S /Q "C:/lib_visual_cpp/opencv_4.7.0"
-echo Remove C:/lib_cpp/opencv_4.7.0
-RD /S /Q "C:/lib_cpp/opencv_4.7.0"
+echo Remove %disk%:/lib_visual_cpp/SDL2
+RD /S /Q "%disk%:/lib_visual_cpp/SDL2"
+echo Remove %disk%:/lib_cpp/SDL2
+RD /S /Q "%disk%:/lib_cpp/SDL2"
+echo Remove %disk%:/lib_visual_cpp/opencv_4.7.0
+RD /S /Q "%disk%:/lib_visual_cpp/opencv_4.7.0"
+echo Remove %disk%:/lib_cpp/opencv_4.7.0
+RD /S /Q "%disk%:/lib_cpp/opencv_4.7.0"
 
 :No
--- vs32_install_all.bat
+++ vs32_install_all.bat
@@ -1,5 +1,45 @@
 @echo off
 
+set install_path=:\lib_visual_cpp
+
+:: ______________Choosing disk______________ ::
+
+:: List availables disks
+set disks=
+
+:: Loop on all possible disks
+SETLOCAL EnableDelayedExpansion
+for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do call :foo %%a 
+goto end
+
+:foo
+if exist %1:\nul (
+    :: If the disk exists, add it to the disks string
+    set disks=!disks!%1 
+)
+:: Return to the for loop
+goto :eof
+
+:: End of for loop
+:end
+
+:: Ask the user to select a disk
+:choice
+set /P disk=Select a disk (%disks%)
+call set disks_without_selection=%%disks:%disk%=%%
+if not x%disks_without_selection%==x%disks% (
+    :: The disk is in the available disks string => OK
+    goto :OK
+) else (
+    :: Else => retry
+    goto :choice
+)
+
+:OK
+
+:: Set the install path
+set install_path=%disk%%install_path%
+
 :: ______________Install SDL______________ ::
 cd ./libraries/SDL
 
@@ -8,8 +48,8 @@ cmake -B ./build -G "Visual Studio 17 2022" -A Win32
 cmake --build ./build --config Release
 
 :: Install SDL at "C:\lib_visual_cpp\SDL2"
-RD /S /Q "C:\lib_visual_cpp\SDL2"
-cmake --install ./build --prefix "C:\lib_visual_cpp\SDL2"
+RD /S /Q "%install_path%\SDL2"
+cmake --install ./build --prefix "%install_path%\SDL2"
 
 :: Remove SDL build
 RD /S /Q "./build"
@@ -23,8 +63,8 @@ cmake -B ./build -G "Visual Studio 17 2022" -A Win32
 cmake --build ./build --config Release
 
 :: Install OpenCV at "C:\lib_visual_cpp\opencv_4.7.0"
-RD /S /Q "C:\lib_visual_cpp\opencv_4.7.0"
-cmake --install ./build --prefix "C:\lib_visual_cpp\opencv_4.7.0"
+RD /S /Q "%install_path%\opencv_4.7.0"
+cmake --install ./build --prefix "%install_path%\opencv_4.7.0"
 
 :: Remove SDL build
 RD /S /Q "./build"
