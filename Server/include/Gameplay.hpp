c30e4bd59e97e34002f5240f098ed4d3d3d0bd6e
Vincent FAUGERE <faugere.vincent@hotmail.fr>
2023-04-20 11:42:49 +0200

    Start of gameplay coding

--- Server/include/Gameplay.hpp
+++ Server/include/Gameplay.hpp
@@ -2,6 +2,11 @@
 
 #include "Player.hpp"
 
+#define PUNCH_DEPTH_SUP_LIMIT 80 // si l'on est en dessus on considère qu'un coup est donné
+#define HEAD_POS_TOLERANCE 2 // tolerance (value+/-tol) pour le coup donné sur la tête 
+#define HP_LOSS 10
+
+
 void gameplay(Player& player1, Player& player2);
 
 
--- Server/src/Gameplay.cpp
+++ Server/src/Gameplay.cpp
@@ -2,6 +2,15 @@
 
 void gameplay(Player& player1, Player& player2) {
 	if ((!player1.getPaused()) && (!player2.getPaused())) {
-
+		if (player1.getLeftHandDepth() >= PUNCH_DEPTH_SUP_LIMIT) {
+			if ((player1.getLeftHandPos() >= player2.getHeadPos() - HEAD_POS_TOLERANCE) && (player1.getLeftHandPos() <= player2.getHeadPos() + HEAD_POS_TOLERANCE)) {
+				if (player2.getLifeBar() > HP_LOSS) {
+					player2.setLifeBar(player2.getLifeBar() - HP_LOSS);
+				}
+				else {
+					// fin du jeu player2 perdu
+				}
+			}
+		}
 	}
 }
