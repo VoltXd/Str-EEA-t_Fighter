11e0c309780ae36b39fac40a35646ea80a9d5f9b
Vincent FAUGERE <faugere.vincent@hotmail.fr>
2023-04-25 15:51:15 +0200

    correction putain de fichier double main

--- Client/include/Vec2D.hpp
+++ Client/include/Vec2D.hpp
@@ -8,22 +8,22 @@ typedef struct Vec2D {
 		this->x = x; this->y = y;
 	}
 
-	Vec2D& operator+=(Vec2D& vec) { return Vec2D(this->x + vec.x, this->y + vec.y); };
+	Vec2D operator+=(Vec2D& vec) { return Vec2D(this->x + vec.x, this->y + vec.y); };
 } Vec2D;
 
-inline Vec2D& operator-(Vec2D& vec1, Vec2D& vec2) {
+inline Vec2D operator-(Vec2D& vec1, Vec2D& vec2) {
 	return Vec2D(vec1.x - vec2.x, vec1.y - vec2.y);
 }
 
-inline Vec2D& operator+(Vec2D& vec1, Vec2D& vec2) {
+inline Vec2D operator+(Vec2D& vec1, Vec2D& vec2) {
 	return Vec2D(vec1.x + vec2.x, vec1.y + vec2.y);
 }
 
-inline Vec2D& operator*(Vec2D& vec, float factor) {
+inline Vec2D operator*(Vec2D& vec, float factor) {
 	return Vec2D(vec.x * factor, vec.y * factor);
 }
 
-inline Vec2D& operator/(Vec2D& vec, float factor) {
+inline Vec2D operator/(Vec2D& vec, float factor) {
 	return Vec2D(vec.x / factor, vec.y / factor);
 }
 
